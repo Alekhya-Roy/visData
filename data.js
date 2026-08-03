4ee1565b98b75b8a1f80eb05111d7b387ad6cba3
Abhishek Roy <alekhyaroy096@gmail.com>
2026-08-03 11:59:22 -0400

    first-commit

---
+++ data.js
@@ -0,0 +1,66 @@
+const data = {
+  "results": [
+    {
+      "gender": "male",
+      "name": {
+        "title": "Mr",
+        "first": "Rafael",
+        "last": "Mitchell"
+      },
+      "location": {
+        "street": {
+          "number": 7984,
+          "name": "Woodlawn Avenue"
+        },
+        "city": "Birr",
+        "state": "Dún Laoghaire–Rathdown",
+        "country": "Ireland",
+        "postcode": 15963,
+        "coordinates": {
+          "latitude": "-3.0649",
+          "longitude": "-133.0338"
+        },
+        "timezone": {
+          "offset": "+8:00",
+          "description": "Beijing, Perth, Singapore, Hong Kong"
+        }
+      },
+      "email": "rafael.mitchell@example.com",
+      "login": {
+        "uuid": "c9dcf131-ef05-45e9-b1b1-1c589ba6a816",
+        "username": "sadbutterfly164",
+        "password": "asterix",
+        "salt": "50cxv6J8",
+        "md5": "349d367af52fad6d49918d4a04ab0a37",
+        "sha1": "2a27d719e15da8d009423abc8fce3f473cefe9cf",
+        "sha256": "574e17d9f03affc9e68d4efe4c29052b7e7fcc5180542f1cf8c5e171eb252194"
+      },
+      "dob": {
+        "date": "1989-11-04T02:56:27.702Z",
+        "age": 36
+      },
+      "registered": {
+        "date": "2008-09-21T01:04:17.213Z",
+        "age": 17
+      },
+      "phone": "051-664-6514",
+      "cell": "081-541-0706",
+      "id": {
+        "name": "PPS",
+        "value": "8888884T"
+      },
+      "picture": {
+        "large": "https://randomuser.me/api/portraits/men/53.jpg",
+        "medium": "https://randomuser.me/api/portraits/med/men/53.jpg",
+        "thumbnail": "https://randomuser.me/api/portraits/thumb/men/53.jpg"
+      },
+      "nat": "IE"
+    }
+  ],
+  "info": {
+    "seed": "8699d99ee24128af",
+    "results": 1,
+    "page": 1,
+    "version": "1.4"
+  }
+}
---
+++ main.js
@@ -0,0 +1,25 @@
+const container = document.getElementById("user-container");
+
+for(let person of data.results){
+
+    const card = document.createElement("div");
+
+    card.classList.add("user-card");
+
+    card.innerHTML = `
+
+        <img src="${person.picture.large}">
+
+        <h2>${person.name.first} ${person.name.last}</h2>
+
+        <p>Email: ${person.email}</p>
+
+        <p>Phone: ${person.phone}</p>
+
+        <p>City: ${person.location.city}</p>
+
+    `;
+
+    container.appendChild(card);
+
+}
